1d96ce9b6cbd6afff3c3062a4c1e8ffd7765174b
Roy Wiggins <roywig@gmail.com>
2025-10-29 19:55:33 +0000

    add signin.tsx

---
+++ src/core/app/signin/Signin.tsx
@@ -0,0 +1,149 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Alert from "@mui/material/Alert";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./Signin.scss";
+// @ts-ignore
+import { SigninDataType } from "../../index";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#580F8B",
+        },
+        secondary: {
+            main: "#6c757d",
+        },
+    },
+});
+
+export default function Signin({
+    signInCallback,
+    appIcon,
+}: {
+    signInCallback: (credentials: SigninDataType) => void;
+    appIcon: string;
+}) {
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        //@ts-ignore
+        signInCallback({
+            email: data.get("email") as string,
+            password: data.get("password") as string,
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div
+                className="flex-center page-root"
+                style={{ paddingTop: "calc(20vh - 20px)" }}
+            >
+                <div id="welcome">
+                    <div id="welcome-logo">
+                        <div
+                            style={{
+                                margin: "auto",
+                                width: "100%",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <img
+                                src={appIcon}
+                                className="img-fluid"
+                                style={{ margin: "auto", height: "70pt" }}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+
+                    <div id="welcome-login">
+                        <Box
+                            component="form"
+                            onSubmit={handleSubmit}
+                            noValidate
+                            style={{ width: "87.5%" }}
+                        >
+                            <TextField
+                                margin="normal"
+                                className="col-md-12"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                size="small"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                size="small"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value="remember"
+                                        size={"small"}
+                                        color="primary"
+                                    />
+                                }
+                                label={
+                                    <Typography variant="subtitle2">
+                                        Remember Me
+                                    </Typography>
+                                }
+                                style={{ float: "right", marginRight: "0" }}
+                                className="input-sm"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="secondary"
+                                sx={{ mb: 2 }}
+                            >
+                                Forgot Password?
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link
+                                        href="#"
+                                        className="btn btn-link"
+                                        variant="body2"
+                                    >
+                                        {"Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </div>
+                </div>
+            </div>
+        </ThemeProvider>
+    );
+}
