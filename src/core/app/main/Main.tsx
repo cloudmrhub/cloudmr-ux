500a958cbdfa48e0bd0ca6005d29b01728ed7ea6
Roy Wiggins <roywig@gmail.com>
2025-10-31 23:08:33 +0000

    dispatch getProfile from getLoggedInToken instead of using useEffect() in Main()

--- src/core/app/main/Main.tsx
+++ src/core/app/main/Main.tsx
@@ -1,32 +1,13 @@
 import { CmrTabs } from "../../../index";
 import type { TabInfo } from "../../../CmrTabs/tab.model";
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { getProfile } from "../../features/authenticate/authenticateActionCreation";
-
 interface MainProps {
   tabData: TabInfo[];
 }
 
 const Main = ({ tabData }: MainProps) => {
-  const [focusedTab, setFocusedTab] = useState(1);
-  const { logged_in_token } = useAppSelector((state) => state.authenticate);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (logged_in_token) {
-      dispatch(getProfile(logged_in_token.accessToken));
-    }
-  }, [logged_in_token, dispatch]);
-
   return (
     <div className="container-fluid mt-4" style={{ transition: "all 0.3s" }}>
-      <CmrTabs
-        tabList={tabData}
-        onTabSelected={(tabIndex) => {
-          setFocusedTab(tabIndex);
-        }}
-      />
+      <CmrTabs tabList={tabData} />
     </div>
   );
 };
