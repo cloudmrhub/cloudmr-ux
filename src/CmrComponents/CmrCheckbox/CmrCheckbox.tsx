f6518ecf8210dd305f4a8c9c205a4c527aa30bce
nguyenxx <xuxi0702@gmail.com>
2026-02-24 16:45:20 -0500

    update checkbox color customization

--- src/CmrComponents/CmrCheckbox/CmrCheckbox.tsx
+++ src/CmrComponents/CmrCheckbox/CmrCheckbox.tsx
@@ -12,27 +12,40 @@ interface CmrCheckboxProps  extends React.HTMLAttributes<HTMLDivElement>{
     onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
     children?: any;
     style?: any;
+    /** Checked state color (e.g. '#580F8B'). Defaults to #580f8b when not set. */
+    checkedColor?: string;
 }
 
 export const CmrCheckbox = (props: CmrCheckboxProps) => {
-    const { defaultChecked, onChange, children, ...rest } = props;
+    const { defaultChecked, onChange, children, checkedColor, ...rest } = props;
+
+    const wrapperStyle: React.CSSProperties = {
+        display: 'contents',
+        ...(checkedColor != null && { ['--cmr-checkbox-checked-color' as string]: checkedColor }),
+    };
 
     return (
-        <FormControlLabel 
-        disabled={props.disabled} 
-        style={props.style} 
-        className={props.className} 
-        control={
-            <Checkbox 
-            style={props.style} 
-            checked={props.checked} 
-            defaultChecked={defaultChecked}
-            onChange={onChange}/>
-        }
-                          
-        label={<span className='cmr-label' style={{paddingRight:0, color:'var(--bs-card-color)'}}>
-        {props.children}</span>}
-        labelPlacement="end"/>
+        <div className="cmr-checkbox-wrapper" style={wrapperStyle}>
+            <FormControlLabel
+                disabled={props.disabled}
+                style={props.style}
+                className={props.className}
+                control={
+                    <Checkbox
+                        style={props.style}
+                        checked={props.checked}
+                        defaultChecked={defaultChecked}
+                        onChange={onChange}
+                    />
+                }
+                label={
+                    <span className="cmr-label" style={{ paddingRight: 0, color: 'var(--bs-card-color)' }}>
+                        {children}
+                    </span>
+                }
+                labelPlacement="end"
+            />
+        </div>
     );
 };
 
